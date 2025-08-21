58891ee2cef691cb0f2d1433b9df40a4bff24539
sanjay-adhikesaven <sanjay.adhikesaven1@gmail.com>
2025-08-21 00:04:48 -0700

    2 ms

--- scripts/combine_experts.sh
+++ scripts/combine_experts.sh
@@ -10,7 +10,7 @@ EXPERT_3=${CHECKPOINTS}/OLMo2-7B-from-posttrained-code-pretrainednonFFN-frozen/s
 # Add other experts
 
 python src/scripts/upcycle/merge_experts_to_flexolmo.py \
-    -m ${PUBLIC_EXPERT} -m ${EXPERT_1} ${EXPERT_2} ${EXPERT_3} \
+    -m ${PUBLIC_EXPERT} ${EXPERT_1} ${EXPERT_2} ${EXPERT_3} \
     -t ${CHECKPOINTS}/OLMo2-7b-flex-base-merged-math-code
 
 # Optional router training on proxy data (provided by data owners)
