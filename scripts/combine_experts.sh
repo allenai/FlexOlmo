e06fee033dd8d7c61cc220cf790312006288f48a
sanjay-adhikesaven <sanjay.adhikesaven1@gmail.com>
2025-08-20 20:52:15 -0700

    comment

--- scripts/combine_experts.sh
+++ scripts/combine_experts.sh
@@ -14,17 +14,16 @@ python src/scripts/upcycle/merge_experts_to_flexolmo.py \
     -t ${CHECKPOINTS}/OLMo2-7b-flex-base-merged-math-code
 
 # Optional router training on proxy data (provided by data owners)
-"""
-torchrun --nproc-per-node=8 src/scripts/train/OLMoE-4x7B.py FlexOlmo-4x7B-RT \
-    --trainer.callbacks.profiler.enabled=true \
-    --dataset.mix_base_dir=${DATA_ROOT} \
-    --dataset.mix=proxy_combined_public_math_code_news \
-    --trainer.max_duration.value=5_000_000_000 \
-    --trainer.max_duration.unit=tokens \
-    --trainer.load_path=${CHECKPOINTS}/FlexOlmo-4x7B \
-    --model.block.feed_forward_moe.router.top_k=4 \
-    --train_module.rank_microbatch_size=4096 \
-    --train_module.scheduler.warmup_steps=100 \
-    --train_module.optim.lr=2e-3 \
-    --trainer.save_folder=${CHECKPOINTS}/FlexOlmo-4x7B-RT
-"""
+
+# torchrun --nproc-per-node=8 src/scripts/train/OLMoE-4x7B.py FlexOlmo-4x7B-RT \
+#    --trainer.callbacks.profiler.enabled=true \
+#    --dataset.mix_base_dir=${DATA_ROOT} \
+#    --dataset.mix=proxy_combined_public_math_code_news \
+#    --trainer.max_duration.value=5_000_000_000 \
+#    --trainer.max_duration.unit=tokens \
+#    --trainer.load_path=${CHECKPOINTS}/FlexOlmo-4x7B \
+#    --model.block.feed_forward_moe.router.top_k=4 \
+#    --train_module.rank_microbatch_size=4096 \
+#    --train_module.scheduler.warmup_steps=100 \
+#    --train_module.optim.lr=2e-3 \
+#    --trainer.save_folder=${CHECKPOINTS}/FlexOlmo-4x7B-RT
