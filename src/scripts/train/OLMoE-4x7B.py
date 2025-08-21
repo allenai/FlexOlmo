7517457ee0a29f4219b81148d439f25c741e6101
sanjay-adhikesaven <sanjay.adhikesaven1@gmail.com>
2025-08-21 16:47:31 -0700

    edit num replicas

--- src/scripts/train/OLMoE-4x7B.py
+++ src/scripts/train/OLMoE-4x7B.py
@@ -87,7 +87,7 @@ def build_train_module_config(common: CommonComponents) -> TransformerTrainModul
             param_dtype=DType.bfloat16,
             reduce_dtype=DType.float32,
             wrapping_strategy=TransformerDataParallelWrappingStrategy.fine_grained,
-            num_replicas=4,  # TODO: set this to number of GPUs / num_experts, 32 when using 8 nodes
+            num_replicas=1,  # TODO: set this to number of GPUs / num_experts, 32 when using 8 nodes
         ),
         # NOTE: expert parallelism requires either HSDP or tensor parallelism.
         ep_config=TransformerExpertParallelConfig(degree=4),
