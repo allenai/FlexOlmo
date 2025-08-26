bcb863e5090c115cea8014aef2b1b41d0b83d613
sanjay-adhikesaven <sanjay.adhikesaven1@gmail.com>
2025-08-25 20:14:11 -0700

    shorter name

--- src/scripts/eval/launch_beaker_eval.sh
+++ src/scripts/eval/launch_beaker_eval.sh
@@ -25,37 +25,37 @@ TASKS=(
     winogrande:mc::olmes
     
     # Gen5 tasks
-    # coqa::olmes
-    # squad::olmes
-    # naturalqs::olmes
-    # triviaqa::olmes
-    # drop::olmes
+    coqa::olmes
+    squad::olmes
+    naturalqs::olmes
+    triviaqa::olmes
+    drop::olmes
 
     # MMLU tasks
-    # mmlu:mc::olmes
-    # mmlu_pro_mc::none
+    mmlu:mc::olmes
+    mmlu_pro_mc::none
 
     # AGI eval
-    # agi_eval_english:1shot::olmes
+    agi_eval_english:1shot::olmes
 
     # BBH
-    # bbh:cot-v1::olmes
+    bbh:cot-v1::olmes
 
     # Math2 tasks
-    # gsm8k::olmes
-    # minerva_math_algebra::olmes
-    # minerva_math_counting_and_probability::olmes
-    # minerva_math_geometry::olmes
-    # minerva_math_intermediate_algebra::olmes
-    # minerva_math_number_theory::olmes
-    # minerva_math_prealgebra::olmes
-    # minerva_math_precalculus::olmes
+    gsm8k::olmes
+    minerva_math_algebra::olmes
+    minerva_math_counting_and_probability::olmes
+    minerva_math_geometry::olmes
+    minerva_math_intermediate_algebra::olmes
+    minerva_math_number_theory::olmes
+    minerva_math_prealgebra::olmes
+    minerva_math_precalculus::olmes
 
     # Code4 tasks
-    # codex_humaneval:temp0.8
-    # codex_humanevalplus:temp0.8
-    # mbpp::none
-    # mbppplus::none
+    codex_humaneval:temp0.8
+    codex_humanevalplus:temp0.8
+    mbpp::none
+    mbppplus::none
 
 )
 
