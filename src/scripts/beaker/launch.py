48637c6e814dc668ad839a921b4e15f128d24795
sanjay-adhikesaven <sanjay.adhikesaven1@gmail.com>
2025-10-22 01:16:06 -0700

    fix commit

--- src/scripts/beaker/launch.py
+++ src/scripts/beaker/launch.py
@@ -81,7 +81,7 @@ def build_launch_config(
             # Setup python environment.
             "conda shell.bash activate base",
             "pip install -e '.[dev,beaker,wandb,train]'",  # we don't need eval, and it causes dependency conflicts
-            "pip uninstall -y olmo-core && pip install git+https://github.com/allenai/OLMo-core.git@682e2011773105dd8e040e4272f1560a61a54540",
+            "pip uninstall -y olmo-core && pip install git+https://github.com/allenai/OLMo-core.git@8f8e97a584fb5b8e78145a8f905efbfec0f4e7ec",
             "pip freeze",
             # Move AWS credentials from env to relevant files
             "mkdir -p ~/.aws",
