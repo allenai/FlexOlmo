20c9c981d5be89dbb92b365527d2f07c354c9d3e
sanjay-adhikesaven <sanjay.adhikesaven1@gmail.com>
2025-10-22 21:25:21 -0700

    mask

--- scripts/combine_experts.sh
+++ scripts/combine_experts.sh
@@ -11,7 +11,7 @@ EXPERT_3=/weka/oe-training-default/ai2-llm/checkpoints/jacobm/flex-olmo/olmo2_fl
 
 python src/scripts/upcycle/dense_to_expert_moe.py \
     -m ${PUBLIC_EXPERT} ${EXPERT_1} ${EXPERT_2} ${EXPERT_3} \
-    -t ${CHECKPOINTS}/OLMo2-7b-flex-base-merged-math-code-experts-sft-math-mixed
+    -t ${CHECKPOINTS}/OLMo2-7b-flex-base-merged-math-code-experts-sft-math-mixed-masked
 
 # Optional router training on proxy data (provided by data owners)
 
