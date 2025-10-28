6c27a52ba9e657b6e43f1792111ea77e986c4047
jacob-morrison <jacobm@allenai.org>
2025-10-28 16:40:49 -0700

    update

--- scripts/create_flex_experts.sh
+++ scripts/create_flex_experts.sh
@@ -2,17 +2,17 @@ GENERAL_MODEL=/weka/oe-training-default/ai2-llm/checkpoints/jacobm/flex-olmo/olm
 PRETRAINED_MODEL=/weka/oe-training-default/ai2-llm/checkpoints/weijias/OLMo2-7B-anneal-from-stage1-no-math/step11921-unsharded
 
 # math expert
-python src/scripts/upcycle/dense_to_expert_moe.py \
-    -m $GENERAL_MODEL \
-       $PRETRAINED_MODEL \
-    -e src/data/domain_embeddings/grit/public.npy \
-       src/data/domain_embeddings/grit/math.npy \
-    -t ../checkpoints/flex-experiments/experts/math-base
-
-# code expert
 # python src/scripts/upcycle/dense_to_expert_moe.py \
 #     -m $GENERAL_MODEL \
 #        $PRETRAINED_MODEL \
 #     -e src/data/domain_embeddings/grit/public.npy \
-#        src/data/domain_embeddings/grit/code.npy \
-#     -t ../checkpoints/flex-experiments/experts/code-base
+#        src/data/domain_embeddings/grit/math.npy \
+#     -t ../checkpoints/flex-experiments/experts/math-base
+
+# code expert
+python src/scripts/upcycle/dense_to_expert_moe.py \
+    -m $GENERAL_MODEL \
+       $PRETRAINED_MODEL \
+    -e src/data/domain_embeddings/grit/public.npy \
+       src/data/domain_embeddings/grit/code.npy \
+    -t ../checkpoints/flex-experiments/experts/code-base
