c864dbb6675ba1ff02755d15814bd791dae28754
jacob-morrison <jacobm@allenai.org>
2025-10-28 21:14:01 -0700

    update

--- scripts/create_flex_experts.sh
+++ scripts/create_flex_experts.sh
@@ -1,13 +1,13 @@
-GENERAL_MODEL=/weka/oe-training-default/ai2-llm/checkpoints/jacobm/flex-olmo/olmo2_flex_base-tulu3-no_code-no_math-dpo-rlvr_step_350
+GENERAL_MODEL=/weka/oe-adapt-default/jacobm/flexolmo/checkpoints/general-model-resized
 PRETRAINED_MODEL=/weka/oe-training-default/ai2-llm/checkpoints/weijias/OLMo2-7B-anneal-from-stage1-no-math/step11921-unsharded
 
 # math expert
-# python src/scripts/upcycle/dense_to_expert_moe.py \
-#     -m $GENERAL_MODEL \
-#        $PRETRAINED_MODEL \
-#     -e src/data/domain_embeddings/grit/public.npy \
-#        src/data/domain_embeddings/grit/math.npy \
-#     -t ../checkpoints/flex-experiments/experts/math-base
+python src/scripts/upcycle/dense_to_expert_moe.py \
+    -m $GENERAL_MODEL \
+       $PRETRAINED_MODEL \
+    -e src/data/domain_embeddings/grit/public.npy \
+       src/data/domain_embeddings/grit/math.npy \
+    -t ../checkpoints/flex-experiments/experts/math-base
 
 # code expert
 python src/scripts/upcycle/dense_to_expert_moe.py \
