1e88b14d0e6648b6ae36377581290f2d88ef83c5
jacob-morrison <jacobm@allenai.org>
2025-11-02 17:00:42 -0800

    test no embeddings

--- scripts/midtrain_my_experts.sh
+++ scripts/midtrain_my_experts.sh
@@ -14,7 +14,9 @@ echo "🚀 Launching Math-2x7B Mid Training..."
 echo ""
 
 MATH_EXPERT=/weka/oe-adapt-default/jacobm/flexolmo/checkpoints/flex-experiments/experts/math-base
+MATH_EXPERT_TEST=/weka/oe-adapt-default/jacobm/flexolmo/checkpoints/flex-experiments/experts/math-base-test
 CODE_EXPERT=/weka/oe-adapt-default/jacobm/flexolmo/checkpoints/flex-experiments/experts/code-base
+SANJAY_EXPERT=/weka/oe-training-default/sanjaya/flexolmo/checkpoints/merged-2x7B-general-math
 
 #    --launch.budget=ai2/oe-base \
 
@@ -28,7 +30,7 @@ python src/scripts/beaker/launch.py launch ai2/jupiter-cirrascale-2 \
    --dataset.mix=mj_finemath4plus \
    --trainer.max_duration.value=50_000_000_000 \
    --trainer.max_duration.unit=tokens \
-   --trainer.load_path=/weka/oe-training-default/sanjaya/flexolmo/checkpoints/merged-2x7B-general-math \
+   --trainer.load_path=${MATH_EXPERT_TEST} \
    --model.block.feed_forward_moe.router.top_k=2 \
    --train_module.rank_microbatch_size=4096 \
    --train_module.scheduler.warmup_steps=2000 \
