0d288c894fe0dfa8fad7301b4a5a71d44fc6b64f
jacob-morrison <jacobm@allenai.org>
2025-11-03 16:09:39 -0800

    test

--- scripts/create_flex_experts.sh
+++ scripts/create_flex_experts.sh
@@ -7,26 +7,26 @@ python src/scripts/upcycle/dense_to_expert_moe.py \
        $PRETRAINED_MODEL \
     -e src/data/domain_embeddings/grit/public.npy \
        src/data/domain_embeddings/grit/math.npy \
-    -t ../checkpoints/flex-experiments/experts/math-base-test3
+    -t /weka/oe-training-default/jacobm/flexolmo/checkpoints/math-base
 
-# # code expert
-# python src/scripts/upcycle/dense_to_expert_moe.py \
-#     -m $GENERAL_MODEL \
-#        $PRETRAINED_MODEL \
-#     -e src/data/domain_embeddings/grit/public.npy \
-#        src/data/domain_embeddings/grit/code.npy \
-#     -t ../checkpoints/flex-experiments/experts/code-base
-
-
-PUBLIC_EXPERT=/weka/oe-training-default/ai2-llm/checkpoints/sanjaya/olmo2-7B-sft/math_expert_sft_mixed/step1062
-EXPERT_1=/weka/oe-training-default/ai2-llm/checkpoints/jacobm/flex-olmo/olmo2_flex_base-tulu3-no_code-no_math-dpo-rlvr_step_350
-EXPERT_2=/weka/oe-training-default/ai2-llm/checkpoints/sanjaya/olmo2-7B-sft/code_expert/step150
-EXPERT_3=/weka/oe-training-default/ai2-llm/checkpoints/jacobm/flex-olmo/olmo2_flex_base-tulu3-no_code-no_math-dpo-rlvr_step_350
-# Add other experts
-
+# code expert
 python src/scripts/upcycle/dense_to_expert_moe.py \
-    -m ${PUBLIC_EXPERT} ${EXPERT_1} \
-    -t /weka/oe-adapt-default/jacobm/flexolmo/checkpoints/flex-experiments/experts/math-base-test-new-conda-env
+    -m $GENERAL_MODEL \
+       $PRETRAINED_MODEL \
+    -e src/data/domain_embeddings/grit/public.npy \
+       src/data/domain_embeddings/grit/code.npy \
+    -t /weka/oe-training-default/jacobm/flexolmo/checkpoints/math-base
+
+
+# PUBLIC_EXPERT=/weka/oe-training-default/ai2-llm/checkpoints/sanjaya/olmo2-7B-sft/math_expert_sft_mixed/step1062
+# EXPERT_1=/weka/oe-training-default/ai2-llm/checkpoints/jacobm/flex-olmo/olmo2_flex_base-tulu3-no_code-no_math-dpo-rlvr_step_350
+# EXPERT_2=/weka/oe-training-default/ai2-llm/checkpoints/sanjaya/olmo2-7B-sft/code_expert/step150
+# EXPERT_3=/weka/oe-training-default/ai2-llm/checkpoints/jacobm/flex-olmo/olmo2_flex_base-tulu3-no_code-no_math-dpo-rlvr_step_350
+# # Add other experts
+
+# python src/scripts/upcycle/dense_to_expert_moe.py \
+#     -m ${PUBLIC_EXPERT} ${EXPERT_1} \
+#     -t /weka/oe-adapt-default/jacobm/flexolmo/checkpoints/flex-experiments/experts/math-base-test-new-conda-env
 
 # python src/scripts/upcycle/dense_to_expert_moe.py \
 #     -m ${PUBLIC_EXPERT} ${EXPERT_1} \
