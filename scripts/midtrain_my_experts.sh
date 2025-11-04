8394afd22291fcefc46172a530acc44e1cb0e677
jacob-morrison <jacobm@allenai.org>
2025-11-03 16:39:37 -0800

    train real models

--- scripts/midtrain_my_experts.sh
+++ scripts/midtrain_my_experts.sh
@@ -22,7 +22,7 @@ CODE_EXPERT=/weka/oe-training-default/jacobm/flexolmo/checkpoints/code-base
 #    --launch.budget=ai2/oe-base \
 
 python src/scripts/beaker/launch.py launch ai2/ceres \
-   --launch.num_nodes=2 \
+   --launch.num_nodes=8 \
    --launch.num_gpus=8 \
    --launch.budget=ai2/oe-base \
    --launch.workspace=ai2/flex2 \
@@ -37,7 +37,25 @@ python src/scripts/beaker/launch.py launch ai2/ceres \
    --train_module.rank_microbatch_size=4096 \
    --train_module.scheduler.warmup_steps=2000 \
    --train_module.optim.lr=9e-4 \
-   --trainer.save_folder=/weka/oe-adapt-default/jacobm/flexolmo/checkpoints/flex-experiments/experts/math-midtrain-test
+   --trainer.save_folder=/weka/oe-training-default/jacobm/flexolmo/checkpoints/math-midtrain
+
+python src/scripts/beaker/launch.py launch ai2/ceres \
+   --launch.num_nodes=8 \
+   --launch.num_gpus=8 \
+   --launch.budget=ai2/oe-base \
+   --launch.workspace=ai2/flex2 \
+   --launch.priority=urgent -- src/scripts/train/OLMoE-2x7B.py Flex-2x7B-code-mid-train \
+   --trainer.callbacks.profiler.enabled=false \
+   --dataset.mix_base_dir=/weka/oe-training-default/ai2-llm/ \
+   --dataset.mix=starcoder \
+   --trainer.max_duration.value=50_000_000_000 \
+   --trainer.max_duration.unit=tokens \
+   --trainer.load_path=${CODE_EXPERT} \
+   --model.block.feed_forward_moe.router.top_k=2 \
+   --train_module.rank_microbatch_size=4096 \
+   --train_module.scheduler.warmup_steps=2000 \
+   --train_module.optim.lr=9e-4 \
+   --trainer.save_folder=/weka/oe-training-default/jacobm/flexolmo/checkpoints/code-midtrain
 
 
 # torchrun --nproc-per-node=8 src/scripts/train/OLMoE-2x7B-anneal.py olmoe-2x7B-${EXPERT}_top2_grit_learnbias \
