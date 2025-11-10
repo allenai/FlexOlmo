7357d8d17643645679039a0f92c2858e220c1e0e
jacob-morrison <jacobm@allenai.org>
2025-11-10 00:57:56 -0800

    test

--- scripts/eval_pretraining.sh
+++ scripts/eval_pretraining.sh
@@ -121,7 +121,7 @@ for MODEL_PATH in "${MODELS[@]}"; do
     gantry run \
         --name $job_name \
         --weka oe-training-default:/weka/oe-training-default \
-        --install "pip install -e \".[all]\"" \
+        --install "pip install -e \".[eval]\"" \
         --budget ai2/oceo \
         --workspace ai2/flex2 \
         --cluster $CLUSTER \
