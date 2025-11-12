a9327e953f80a4dce723c2ec2b09ce14b21866f6
jacob-morrison <jacobm@allenai.org>
2025-11-11 22:57:17 -0800

    update

---
+++ src/offline_evals/tasks/__init__.py
@@ -0,0 +1,15 @@
+from typing import Dict
+
+from oe_eval.tasks.oe_eval_tasks import TASK_REGISTRY
+
+from .tasks import (
+    squad,
+    squad2,
+)
+
+new_task_registry: Dict = {
+    "squad": squad.SQuAD,
+    "squad2": squad2.SQuAD2,
+}
+
+TASK_REGISTRY.update(new_task_registry)
