f7fd37813aafde57c0791df87dae235d95b9747d
jacob-morrison <jacobm@allenai.org>
2025-11-11 22:28:36 -0800

    update

--- scripts/midtrain_my_experts.sh
+++ scripts/midtrain_my_experts.sh
@@ -21,7 +21,7 @@ CODE_EXPERT=/weka/oe-training-default/jacobm/flexolmo/checkpoints/code-base
 
 #    --launch.budget=ai2/oe-base \
 
-# python src/scripts/beaker/launch.py launch ai2/jupiter \
+# uv python src/scripts/beaker/launch.py launch ai2/jupiter \
 #    --launch.num_nodes=8 \
 #    --launch.num_gpus=8 \
 #    --launch.budget=ai2/oceo \
