ba12ee6d9d482dc5ecb3264942d7057df96f67f8
jacob-morrison <jacobm@allenai.org>
2025-11-13 16:57:58 -0800

    test

--- scripts/eval_pretraining.sh
+++ scripts/eval_pretraining.sh
@@ -19,49 +19,49 @@ model_type=hf
 
 # Define all available tasks from run_eval.sh (ALL tasks from all groups)
 TASKS=(
-    # MC9 tasks
-    arc_easy:mc::olmes
-    arc_challenge:mc::olmes
-    boolq:mc::olmes
-    csqa:mc::olmes
-    hellaswag:mc::olmes
-    openbookqa:mc::olmes
-    piqa:mc::olmes
-    socialiqa:mc::olmes
-    winogrande:mc::olmes
+    # # MC9 tasks
+    # arc_easy:mc::olmes
+    # arc_challenge:mc::olmes
+    # boolq:mc::olmes
+    # csqa:mc::olmes
+    # hellaswag:mc::olmes
+    # openbookqa:mc::olmes
+    # piqa:mc::olmes
+    # socialiqa:mc::olmes
+    # winogrande:mc::olmes
     
-    # Gen5 tasks
-    coqa::olmes
+    # # Gen5 tasks
+    # coqa::olmes
     squad::olmes
-    naturalqs::olmes
-    triviaqa::olmes
-    drop::olmes
+    # naturalqs::olmes
+    # triviaqa::olmes
+    # drop::olmes
 
-    # MMLU tasks
-    mmlu:mc::olmes
-    mmlu_pro_mc::none
+    # # MMLU tasks
+    # mmlu:mc::olmes
+    # mmlu_pro_mc::none
 
-    # AGI eval
-    agi_eval_english:1shot::olmes
+    # # AGI eval
+    # agi_eval_english:1shot::olmes
 
-    # BBH
-    bbh:cot-v1::olmes
+    # # BBH
+    # bbh:cot-v1::olmes
 
-    # Math2 tasks
-    gsm8k::olmes
-    minerva_math_algebra::olmes
-    minerva_math_counting_and_probability::olmes
-    minerva_math_geometry::olmes
-    minerva_math_intermediate_algebra::olmes
-    minerva_math_number_theory::olmes
-    minerva_math_prealgebra::olmes
-    minerva_math_precalculus::olmes
+    # # Math2 tasks
+    # gsm8k::olmes
+    # minerva_math_algebra::olmes
+    # minerva_math_counting_and_probability::olmes
+    # minerva_math_geometry::olmes
+    # minerva_math_intermediate_algebra::olmes
+    # minerva_math_number_theory::olmes
+    # minerva_math_prealgebra::olmes
+    # minerva_math_precalculus::olmes
 
-    # Code4 tasks
-    codex_humaneval:temp0.8
-    codex_humanevalplus:temp0.8
-    mbpp::none
-    mbppplus::none
+    # # Code4 tasks
+    # codex_humaneval:temp0.8
+    # codex_humanevalplus:temp0.8
+    # mbpp::none
+    # mbppplus::none
 )
 
 # Function to get checkpoint name (matching the original script)
