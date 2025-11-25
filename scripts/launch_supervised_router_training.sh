104881dd7f0463b92db60d0edb86698a9c9df7ad
sanjay-adhikesaven <sanjay.adhikesaven1@gmail.com>
2025-11-25 11:25:31 -0800

    new name

--- scripts/launch_supervised_router_training.sh
+++ scripts/launch_supervised_router_training.sh
@@ -14,7 +14,7 @@ python src/scripts/beaker/launch.py launch ai2/jupiter-cirrascale-2 \
    --trainer.max_duration.value=5000000000 \
    --trainer.max_duration.unit=tokens \
    --trainer.load_path=/weka/oe-training-default/sanjaya/flexolmo/checkpoints/OLMo2-7b-flex-base-merged-math-code \
-   --trainer.save_folder=/weka/oe-training-default/sanjaya/flexolmo/checkpoints/OLMo2-7b-flex-base-merged-math-code-RT-supervised \
+   --trainer.save_folder=/weka/oe-training-default/sanjaya/flexolmo/checkpoints/OLMo2-7b-flex-base-merged-math-code-RT-supervised-2 \
    --model.block.feed_forward_moe.num_experts=4 \
    --model.block.feed_forward_moe.router.top_k=4 \
    --train_module.rank_microbatch_size=4096 \
