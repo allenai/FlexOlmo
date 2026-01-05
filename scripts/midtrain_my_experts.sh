797a620c393a3609a49d38e91ba7c8d9de21b705
jacob-morrison <jacobm@allenai.org>
2026-01-05 12:28:59 -0800

    update

--- scripts/midtrain_my_experts.sh
+++ scripts/midtrain_my_experts.sh
@@ -21,45 +21,46 @@ CODE_EXPERT=/weka/oe-training-default/jacobm/flexolmo/checkpoints/code-base
 #    --launch.budget=ai2/oe-base \
 
 MATH_EXPERT=/weka/oe-training-default/jacobm/flexolmo/checkpoints/math-base
-uv run python src/scripts/beaker/launch.py launch ai2/jupiter \
-   --launch.num_nodes=8 \
-   --launch.num_gpus=8 \
-   --launch.budget=ai2/oceo \
-   --launch.workspace=ai2/flex2 \
-   --launch.priority=urgent -- src/scripts/train/OLMoE-2x7B-anneal.py Flex-2x7B-math-anneal-NO-frozen-router-5b \
-   --trainer.callbacks.profiler.enabled=false \
-   --dataset.mix_base_dir=/weka/oe-training-default/ai2-llm/ \
-   --dataset.mix=mj_finemath4plus \
-   --trainer.max_duration.value=5_000_000_000 \
-   --trainer.max_duration.unit=tokens \
-   --trainer.load_path=${MATH_EXPERT} \
-   --model.block.feed_forward_moe.router.top_k=2 \
-   --train_module.rank_microbatch_size=4096 \
-   --train_module.scheduler.warmup_steps=2000 \
-   --train_module.optim.lr=9e-4 \
-   --trainer.save_folder=/weka/oe-training-default/jacobm/flexolmo/checkpoints/math-math-anneal-NO-frozen-router-5b
+
+# uv run python src/scripts/beaker/launch.py launch ai2/jupiter \
+#    --launch.num_nodes=8 \
+#    --launch.num_gpus=8 \
+#    --launch.budget=ai2/oceo \
+#    --launch.workspace=ai2/flex2 \
+#    --launch.priority=urgent -- src/scripts/train/OLMoE-2x7B-anneal.py Flex-2x7B-math-anneal-NO-frozen-router-5b \
+#    --trainer.callbacks.profiler.enabled=false \
+#    --dataset.mix_base_dir=/weka/oe-training-default/ai2-llm/ \
+#    --dataset.mix=mj_finemath4plus \
+#    --trainer.max_duration.value=5_000_000_000 \
+#    --trainer.max_duration.unit=tokens \
+#    --trainer.load_path=${MATH_EXPERT} \
+#    --model.block.feed_forward_moe.router.top_k=2 \
+#    --train_module.rank_microbatch_size=4096 \
+#    --train_module.scheduler.warmup_steps=2000 \
+#    --train_module.optim.lr=9e-4 \
+#    --trainer.save_folder=/weka/oe-training-default/jacobm/flexolmo/checkpoints/math-math-anneal-NO-frozen-router-5b
 
 # Code-2x7B Mid Training
 echo "🚀 Launching Code-2x7B Mid Training..."
 echo ""
 
-python src/scripts/beaker/launch.py launch ai2/jupiter \
-   --launch.num_nodes=4 \
+uv run python src/scripts/beaker/launch.py launch ai2/jupiter \
+   --launch.num_nodes=8 \
    --launch.num_gpus=8 \
    --launch.budget=ai2/oceo \
    --launch.workspace=ai2/flex2 \
-   --launch.priority=urgent -- src/scripts/train/OLMoE-2x7B-anneal.py Flex-2x7B-code-anneal \
+   --launch.priority=urgent -- src/scripts/train/OLMoE-2x7B-anneal.py Flex-2x7B-code-anneal-5B \
    --trainer.callbacks.profiler.enabled=false \
    --dataset.mix_base_dir=/weka/oe-training-default/ai2-llm/ \
    --dataset.mix=starcoder \
-   --trainer.max_duration.value=50_000_000_000 \
+   --trainer.max_duration.value=5_000_000_000 \
    --trainer.max_duration.unit=tokens \
    --trainer.load_path=${CODE_EXPERT} \
    --model.block.feed_forward_moe.router.top_k=2 \
    --train_module.rank_microbatch_size=4096 \
    --train_module.scheduler.warmup_steps=2000 \
    --train_module.optim.lr=9e-4 \
-   --trainer.save_folder=/weka/oe-training-default/jacobm/flexolmo/checkpoints/code-anneal-no-expert-bias
+   --trainer.save_folder=/weka/oe-training-default/jacobm/flexolmo/checkpoints/code-anneal-no-expert-bias-5B
 
 
 # torchrun --nproc-per-node=8 src/scripts/train/OLMoE-2x7B-anneal.py olmoe-2x7B-${EXPERT}_top2_grit_learnbias \
