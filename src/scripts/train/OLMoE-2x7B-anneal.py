2facbda8171cbc185eaad1c9b88d561bf97689c5
jacob-morrison <jacobm@allenai.org>
2026-01-05 13:28:24 -0800

    test not setting an lb loss weight

--- src/scripts/train/OLMoE-2x7B-anneal.py
+++ src/scripts/train/OLMoE-2x7B-anneal.py
@@ -47,7 +47,7 @@ def build_model_config(common: CommonComponents) -> TransformerConfig:
     return TransformerConfig.olmoe_nx7b_with_expert_bias(  # type: ignore
         vocab_size=common.tokenizer.padded_vocab_size(),
         num_experts=2,
-        lb_loss_weight=0,
+        # lb_loss_weight=0,
         z_loss_weight=0.001,
         freeze_params=[
             "embeddings.*",
