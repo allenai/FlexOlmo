5533e83af3f91d9ce50ea4284aa03d16e938452e
jacob-morrison <jacobm@allenai.org>
2026-01-05 14:11:32 -0800

    roll back changes

--- src/flexolmo/internal/model_utils.py
+++ src/flexolmo/internal/model_utils.py
@@ -37,7 +37,7 @@ def olmoe_nx7b(cls, vocab_size: int, **kwargs) -> "TransformerConfig":
         vocab_size=vocab_size,
         dropless=dropless,
         capacity_factor=None if dropless else 1.2,  # adjust as needed
-        # lb_loss_weight=kwargs.pop("lb_loss_weight", 0.01),
+        lb_loss_weight=kwargs.pop("lb_loss_weight", 0.01),
         z_loss_weight=kwargs.pop("z_loss_weight", 0.001),
         reordered_norm=kwargs.pop("reordered_norm", True),
         qk_norm=kwargs.pop("qk_norm", True),
