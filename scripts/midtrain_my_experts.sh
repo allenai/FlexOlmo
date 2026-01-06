18b66c677faf8439c5f421fb06b072e2e5c4748e
jacob-morrison <jacobm@allenai.org>
2026-01-05 21:17:04 -0800

    50b code

--- scripts/midtrain_my_experts.sh
+++ scripts/midtrain_my_experts.sh
@@ -49,18 +49,18 @@ uv run python src/scripts/beaker/launch.py launch ai2/jupiter \
    --launch.num_gpus=8 \
    --launch.budget=ai2/oceo \
    --launch.workspace=ai2/flex2 \
-   --launch.priority=urgent -- src/scripts/train/OLMoE-2x7B-anneal.py Flex-2x7B-olmo3-code-anneal-5B \
+   --launch.priority=urgent -- src/scripts/train/OLMoE-2x7B-anneal.py Flex-2x7B-olmo3-code-anneal-50B \
    --trainer.callbacks.profiler.enabled=false \
    --dataset.mix_base_dir=/weka/oe-training-default/ai2-llm/ \
    --dataset.mix=olmo3_code \
-   --trainer.max_duration.value=5_000_000_000 \
+   --trainer.max_duration.value=50_000_000_000 \
    --trainer.max_duration.unit=tokens \
    --trainer.load_path=${CODE_EXPERT} \
    --model.block.feed_forward_moe.router.top_k=2 \
    --train_module.rank_microbatch_size=4096 \
    --train_module.scheduler.warmup_steps=2000 \
    --train_module.optim.lr=9e-4 \
-   --trainer.save_folder=/weka/oe-training-default/jacobm/flexolmo/checkpoints/olmo3-code-anneal-5B
+   --trainer.save_folder=/weka/oe-training-default/jacobm/flexolmo/checkpoints/olmo3-code-anneal-50B
 
 
 # torchrun --nproc-per-node=8 src/scripts/train/OLMoE-2x7B-anneal.py olmoe-2x7B-${EXPERT}_top2_grit_learnbias \
