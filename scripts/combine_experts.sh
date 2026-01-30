ea14df83581c40664e46cc41c37a6e797282d330
sanjay-adhikesaven <sanjay.adhikesaven1@gmail.com>
2026-01-30 12:52:43 -0800

    script

--- scripts/combine_experts.sh
+++ scripts/combine_experts.sh
@@ -10,9 +10,9 @@ EXPERT_3=/weka/oe-training-default/ai2-llm/checkpoints/jacobm/flex-olmo/olmo2_fl
 # Add other experts
 
 # Create 4x7B model by merging all 4 experts
-python src/scripts/upcycle/merge_experts_to_flexolmo.py \
-    --models ${PUBLIC_EXPERT} ${EXPERT_1} ${EXPERT_2} ${EXPERT_3} \
-    --target ${CHECKPOINTS}/OLMo2-7b-flex-base-merged-4x7B-pretrained-experts-olmo3code
+python src/scripts/upcycle/dense_to_expert_moe.py \
+    -m ${PUBLIC_EXPERT} ${EXPERT_1} ${EXPERT_2} ${EXPERT_3} \
+    -t ${CHECKPOINTS}/OLMo2-7b-flex-base-merged-4x7B-pretrained-experts-olmo3code
 
 # Optional router training on proxy data (provided by data owners)
 
