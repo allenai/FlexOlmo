a2df663d43c4e9ce0174cdfcdfc01b7c92cb069d
sanjay-adhikesaven <sanjay.adhikesaven1@gmail.com>
2026-01-30 20:52:59 -0800

    ok

--- scripts/combine_experts.sh
+++ scripts/combine_experts.sh
@@ -3,16 +3,16 @@
 # export DATA_ROOT="http://flexolmo-data.org"
 export CHECKPOINTS=/weka/oe-training-default/sanjaya/flexolmo/checkpoints
 
-PUBLIC_EXPERT=/weka/oe-training-default/ai2-llm/checkpoints/sanjaya/olmo2-7B-sft/math_expert_sft_mixed/step1062
+PUBLIC_EXPERT=/weka/oe-training-default/sanjaya/flexolmo/checkpoints/OLMo2-7B-from-posttrained-math-pretrainednonFFN-frozen/step11921
 EXPERT_1=/weka/oe-training-default/ai2-llm/checkpoints/jacobm/flex-olmo/olmo2_flex_base-tulu3-no_code-no_math-dpo-rlvr_step_350
-EXPERT_2=/weka/oe-training-default/ai2-llm/checkpoints/sanjaya/olmo2-7B-sft/code_expert_sft_new/step620
+EXPERT_2=/weka/oe-training-default/sanjaya/flexolmo/checkpoints/OLMo2-7B-from-posttrained-olmo3-code-pretrainednonFFN-frozen/step11921
 EXPERT_3=/weka/oe-training-default/ai2-llm/checkpoints/jacobm/flex-olmo/olmo2_flex_base-tulu3-no_code-no_math-dpo-rlvr_step_350
 # Add other experts
 
 # Create 4x7B model by merging all 4 experts
 python src/scripts/upcycle/dense_to_expert_moe.py \
     -m ${PUBLIC_EXPERT} ${EXPERT_1} ${EXPERT_2} ${EXPERT_3} \
-    -t ${CHECKPOINTS}/OLMo2-7b-flex-base-merged-4x7B-sft-experts-olmo3code
+    -t ${CHECKPOINTS}/OLMo2-7b-flex-base-merged-4x7B-pretrained-experts-olmo3code
 
 # Optional router training on proxy data (provided by data owners)
 
