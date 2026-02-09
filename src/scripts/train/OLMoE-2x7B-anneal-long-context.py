49ceb7e6c3230cf93cc97c0ed6df0b49da3d6f2e
jacob-morrison <jacobm@allenai.org>
2026-02-09 14:24:09 -0800

    oops I messed up compilation

--- src/scripts/train/OLMoE-2x7B-anneal-long-context.py
+++ src/scripts/train/OLMoE-2x7B-anneal-long-context.py
@@ -38,7 +38,7 @@ from flexolmo.train.train_module.transformer import (
     FreezeTransformerTrainModuleConfig,
 )
 
-SEQUENCE_LENGTH = 4096 # 32768
+SEQUENCE_LENGTH = 32768
 
 log = logging.getLogger(__name__)
 
