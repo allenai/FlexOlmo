e8536bc5197c5016f5b9e1eb2090f8e33ab409e8
jacob-morrison <jacobm@allenai.org>
2026-02-10 12:51:16 -0800

    test

--- src/scripts/beaker/launch.py
+++ src/scripts/beaker/launch.py
@@ -83,7 +83,7 @@ def build_launch_config(
             # Setup python environment.
             "conda shell.bash activate base",
             "pip install -e '.[dev,beaker,wandb,train]'",  # we don't need eval, and it causes dependency conflicts
-            "pip install --upgrade flash-attn --no-build-isolation",
+            "pip install 'https://github.com/Dao-AILab/flash-attention/releases/download/v2.8.3/flash_attn-2.8.3+cu12torch2.8cxx11abiFALSE-cp311-cp311-linux_x86_64.whl",
             "pip freeze",
             # Move AWS credentials from env to relevant files
             "mkdir -p ~/.aws",
