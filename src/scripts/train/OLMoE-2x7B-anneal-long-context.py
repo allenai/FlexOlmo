48ccbe525087f09b8d0a062176d7d1a2c7471156
jacob-morrison <jacobm@allenai.org>
2026-02-15 16:09:36 -0800

    test cp at 8k

--- src/scripts/train/OLMoE-2x7B-anneal-long-context.py
+++ src/scripts/train/OLMoE-2x7B-anneal-long-context.py
@@ -45,7 +45,7 @@ from flexolmo.train.train_module.transformer import (
     FreezeTransformerTrainModuleConfig,
 )
 
-SEQUENCE_LENGTH = 16384 # 32768 # 65536
+SEQUENCE_LENGTH = 8192 # 16384 # 32768 # 65536
 BATCH_SIZE = 1048576
 CP_DEGREE = 4
 
