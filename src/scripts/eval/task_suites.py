ffec13d071927a4008048564ed6bd1df741a70c9
Kevin Farhat <kevinfarhat@allenai.org>
2026-02-19 14:24:24 -0800

    add task suite for mmlu pro

--- src/scripts/eval/task_suites.py
+++ src/scripts/eval/task_suites.py
@@ -1,9 +1,14 @@
 from oe_eval.configs.task_suites import TASK_SUITE_CONFIGS
+from oe_eval.data.mmlu_pro_categories import MMLU_PRO_CATEGORIES
 
 
 def get_task_suite_configs():
     TASK_SUITE_CONFIGS.update(
         {
+            "mmlu_pro:mc": {
+                "tasks": [f"mmlu_pro_{cat}:mc::none" for cat in MMLU_PRO_CATEGORIES],
+                "primary_metric": "macro",
+            },
             "sciriff5": {
                 "tasks": [
                     "sciriff_bioasq_factoid_qa",  # Abstractive
