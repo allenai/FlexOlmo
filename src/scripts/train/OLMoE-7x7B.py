3d0d63a29ab5443b2d0c732cbd8ab4079fc24bb5
Akshita Bhagia <akshita23bhagia@gmail.com>
2026-02-23 15:25:26 -0800

    w/o expert bias

--- src/scripts/train/OLMoE-7x7B.py
+++ src/scripts/train/OLMoE-7x7B.py
@@ -45,7 +45,7 @@ log = logging.getLogger(__name__)
 
 
 def build_model_config(common: CommonComponents) -> TransformerConfig:
-    return TransformerConfig.olmoe_nx7b_with_expert_bias(  # type: ignore
+    return TransformerConfig.olmoe_nx7b(  # type: ignore
         vocab_size=common.tokenizer.padded_vocab_size(),
         num_experts=7,
         top_k=7,
