82b2d273f98e813aed040ca7c11ff560f0c9a88c
jacob-morrison <jacobm@allenai.org>
2026-03-10 16:30:26 -0700

    delete med data mix

--- scripts/jacobs_scripts/midtrain_my_experts.sh
+++ scripts/jacobs_scripts/midtrain_my_experts.sh
@@ -26,7 +26,7 @@ uv run python src/scripts/beaker/launch.py launch ai2/jupiter \
    --launch.num_gpus=8 \
    --launch.budget=ai2/oceo \
    --launch.workspace=ai2/flex2 \
-   --launch.priority=urgent -- src/scripts/train/OLMoE-2x7B-anneal.py flex-2x7B-olmo3_math_anneal-50b-8k \
+   --launch.priority=urgent -- src/scripts/train/OLMoE-2x7B-anneal-8k.py flex-2x7B-olmo3_math_anneal-50b-8k \
    --trainer.callbacks.profiler.enabled=false \
    --dataset.mix_base_dir=/weka/oe-training-default/ai2-llm/ \
    --dataset.mix=olmo3_math \
