3fe49f644531fa7aff549360cfc476764bd80802
jacob-morrison <jacobm@allenai.org>
2026-03-15 21:46:53 -0700

    fix

--- src/flexolmo/data/mixes/__init__.py
+++ src/flexolmo/data/mixes/__init__.py
@@ -61,7 +61,7 @@ class CustomDataMix(DataMixBase):
     long_context = "long_context"
     olmo3_long_context_debug = "olmo3_long_context_debug"
     olmo3_reasoning = "olmo3_reasoning"
-    kevin_med_data = "kevin_med_data"
+    pmc = "pmc"
     
     # glob-based mixtures
     code_glob = "code_glob"
